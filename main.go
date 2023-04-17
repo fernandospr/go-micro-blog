7cb83b988aaacdcf67b6de553c8fbf1ecb2a6ea2
Fernando Sproviero <fernandospr@gmail.com>
2023-04-17 11:40:30 -0300

    Refactoring to a models package

--- main.go
+++ main.go
@@ -1,61 +1,41 @@
 package main
 
 import (
+	"go-micro-blog/models"
+
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Post struct {
-	User string
-	Text string
-}
-
-var posts []Post
-
 func getPosts(c *gin.Context) {
-	c.JSON(http.StatusOK, posts)
+	c.JSON(http.StatusOK, models.Posts())
 }
 
 func getPostsByUser(c *gin.Context) {
 	user := c.Param("user")
 
-	postsFromUser := filterPostsByUser(user)
+	postsFromUser := models.FilterPostsByUser(user)
 
 	c.JSON(http.StatusOK, postsFromUser)
 }
 
-func filterPostsByUser(user string) *[]Post {
-	postsFromUser := []Post{}
-	for _, p := range posts {
-		if p.User == user {
-			postsFromUser = append(postsFromUser, p)
-		}
-	}
-	return &postsFromUser
-}
-
 func addPost(c *gin.Context) {
-	var newPost Post
+	var newPost models.Post
 	if err := c.ShouldBindJSON(&newPost); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	posts = append(posts, newPost)
+	models.AddPost(newPost)
 
 	c.JSON(http.StatusCreated, newPost)
 }
 
 func main() {
-	r := gin.Default()
+	models.Init()
 
-	posts = []Post{
-		{User: "user1", Text: "Hello World"},
-		{User: "user2", Text: "Hola Mundo"},
-		{User: "user1", Text: "Other message from user"},
-		{User: "user2", Text: "Hello World"},
-	}
+	r := gin.Default()
 
 	r.GET("/posts", getPosts)
 	r.GET("/posts/:user", getPostsByUser)
---
+++ models/post.go
@@ -0,0 +1,35 @@
+package models
+
+type Post struct {
+	User string
+	Text string
+}
+
+var posts []Post
+
+func Posts() *[]Post {
+	return &posts
+}
+
+func Init() {
+	posts = []Post{
+		{User: "user1", Text: "Hello World"},
+		{User: "user2", Text: "Hola Mundo"},
+		{User: "user1", Text: "Other message from user"},
+		{User: "user2", Text: "Hello World"},
+	}
+}
+
+func AddPost(newPost Post) {
+	posts = append(posts, newPost)
+}
+
+func FilterPostsByUser(user string) *[]Post {
+	postsFromUser := []Post{}
+	for _, p := range posts {
+		if p.User == user {
+			postsFromUser = append(postsFromUser, p)
+		}
+	}
+	return &postsFromUser
+}
